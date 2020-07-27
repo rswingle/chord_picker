af598325135857ebe14d67eca891e7126e74f95d
rswingle <ray.swingle@gmail.com>
2020-07-27 09:14:18 -0400

    fixed notes

--- chords.py
+++ chords.py
@@ -2,6 +2,6 @@
 
 from random import *
 
-notes = ['A','Bb','C','C#','D','D#','E','F','F#','G','G#']
+notes = ['A','Bb','B','C','C#','D','D#','E','F','F#','G','G#']
 x = sample(notes, randint(3,4))
 print(x)
