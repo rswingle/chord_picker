d046b583dd12ee4f1ee5ec50664e3f2c9d8cda64
rswingle <ray.swingle@gmail.com>
2020-08-03 12:05:21 -0400

    started work to support intervals

--- chords.py
+++ chords.py
@@ -3,5 +3,22 @@
 from random import *
 
 notes = ['A','Bb','B','C','C#','D','D#','E','F','F#','G','G#']
+
+#intervals = ['I','II','III','IV','V','VI','VII']
+#Define what intervals sound right for rock
+def interval(prog):
+    inter = [['I,III','V'],
+    ['I','IV','V'],
+    ['I','IV','V','VI'],
+    ['I','V','IV','IV'],
+    ['I','IV','V'],
+    ['V','IV','I'],
+    ['I','V','iv','IV']
+    ]
+
+
 x = sample(notes, randint(3,4))
+
+#if interval(x) "matches"
+
 print(x)
